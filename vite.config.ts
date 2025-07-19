0fad1c347635bde9bbda9e8261997854b2c4e5c5
Priyanka S <priyaselvaraj2431@gmail.com>
2025-07-19 14:45:06 +0530

    Update portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/priya-portfolio-motion-main/',
+  base: '/Priyanka-S-Portfolio/',
   server: {
     host: "::",
     port: 8080,
